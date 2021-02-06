37540321ab89da8d1a7505c0c13abb849ed85e98
Song <vvright@163.com>
2021-02-07 00:13:57 +0800

    refactor BallroomDanceRootine from BallroomDance

--- dancepose/src/test/java/dance/BallroomDance.java
+++
@@ -1,7 +0,0 @@
-package dance;
-
-class BallroomDance {
-    public static void main(String[] args) {
-        System.out.println("BallroomDance");
-    }
-}
---
+++ dancepose/src/test/java/dance/BallroomDanceRootine.java
@@ -0,0 +1,10 @@
+package dance;
+
+class BallroomDanceRootine {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ballroom Dance Rootine");
+
+    }
+}
