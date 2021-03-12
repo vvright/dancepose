301e270fe93f6fd89b1a71ced5200ec657d6dc3c
Song <vvright@163.com>
2021-03-12 22:06:49 +0800

    add basic Waltz figures

---
+++ dancepose/src/main/java/dance/ballroom/figures/W/LeftWhisk.java
@@ -0,0 +1,9 @@
+package dance.ballroom.figures.W;
+
+import dance.ballroom.figures.Figure;
+
+/**
+ * @author Song
+ */
+public class LeftWhisk extends Figure {
+}
---
+++ dancepose/src/main/java/dance/ballroom/figures/W/NaturalPivot.java
@@ -0,0 +1,9 @@
+package dance.ballroom.figures.W;
+
+import dance.ballroom.figures.Figure;
+
+/**
+ * @author Song
+ */
+public class NaturalPivot extends Figure {
+}
