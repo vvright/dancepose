33dcab0b12aea3366f08342abaa9c58aaeb6687b
Song <vvright@163.com>
2021-03-23 21:56:39 +0800

    refactor directories and add some basic figures of Rumba

---
+++ dancepose/src/main/java/dance/latin/figures/Rumba/ProgressiveWalks.java
@@ -0,0 +1,9 @@
+package dance.latin.figures.Rumba;
+
+import dance.ballroom.figures.Figure;
+
+/**
+ * @author Song
+ */
+public class ProgressiveWalks extends Figure {
+}
---
+++ dancepose/src/main/java/dance/latin/figures/Rumba/ShoulderToShoulder.java
@@ -0,0 +1,10 @@
+package dance.latin.figures.Rumba;
+
+import dance.ballroom.figures.Figure;
+
+/**
+ * @author Song
+ */
+public class ShoulderToShoulder extends Figure {
+}
+
